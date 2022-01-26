dee61e95bf3b4ba0714bda22ebedf7a896333887
ashish0599 <96234443+ashish0599@users.noreply.github.com>
2022-01-26 15:23:40 +0530

    day-21-task

---
+++ script.js
@@ -0,0 +1,89 @@
+function one(callback) {
+    setTimeout(() => {
+        document.getElementById("myid").innerHTML = 1;
+      callback();
+    }, 1000);
+  };
+  function two(callback) {
+    setTimeout(() => {
+        document.getElementById("myid").innerHTML = 2;
+      callback();
+    }, 1000);
+  };
+  function three(callback) {
+    setTimeout(() => {
+        document.getElementById("myid").innerHTML = 3;
+      callback();
+    }, 1000);
+  };
+  function four(callback) {
+    setTimeout(() => {
+        document.getElementById("myid").innerHTML = 4;
+      callback();
+    }, 1000);
+  };
+  function five(callback) {
+    setTimeout(() => {
+        document.getElementById("myid").innerHTML = 5;
+      callback();
+    }, 1000);
+  };
+  
+  function six(callback) {
+    setTimeout(() => {
+        document.getElementById("myid").innerHTML = 6;
+      callback();
+    }, 1000);
+  };
+  
+  function seven(callback) {
+    setTimeout(() => {
+        document.getElementById("myid").innerHTML = 7;
+      callback();
+    }, 1000);
+  };
+  
+  function eight(callback) {
+    setTimeout(() => {
+        document.getElementById("myid").innerHTML = 8;
+      callback();
+    }, 1000);
+  };
+  
+  function nine(callback) {
+    setTimeout(() => {
+        document.getElementById("myid").innerHTML = 9;
+      callback();
+    }, 1000);
+  };
+  
+  function ten(callback) {
+    setTimeout(() => {
+        document.getElementById("myid").innerHTML = 10;
+      callback();
+    }, 1000);
+  };
+  
+  ten(() => {
+    nine(() => {
+        eight (() => {
+            seven(() => {
+                six(() => {
+                    five(() => {
+                        four(() => {
+                            three(() => {
+                                two(() => {
+                                    one(() => {
+                                        setTimeout(() =>{
+                                            document.getElementById("myid").innerHTML = "Happy Independence Day";
+                                        }, 2000);                                        
+                                    })
+                                })
+                            })
+                        })
+                    })
+                })
+            })
+        })
+    })
+  });
